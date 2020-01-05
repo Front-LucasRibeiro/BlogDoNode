675700ff0b8e40518bb421c0cb044854d437a515
Lucas Ribeiro <lksribeiro2014@gmail.com>
2020-01-05 02:39:57 -0300

    [FIX] ajuste arquivo db

--- config/db.js
+++ config/db.js
@@ -1,15 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://LucasLks:fr0nt3nd2019@cluster0-qsqrm.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: uri}
+  module.exports = {mongoURI: 'mongodb://LucasLks:fr0nt3nd2019@cluster0-qsqrm.mongodb.net/test?retryWrites=true&w=majority'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
-
