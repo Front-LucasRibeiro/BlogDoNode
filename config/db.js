02861e100bcbd410e5eb8948a3ea48fc04784a64
Lucas Ribeiro <lksribeiro2014@gmail.com>
2020-01-05 03:37:17 -0300

    mudanca nome collection

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://LucasLks:fr0nt3nd2019@cluster0-qsqrm.mongodb.net/test?retryWrites=true&w=majority'}
+  module.exports = {mongoURI: 'mongodb+srv://LucasLks:fr0nt3nd2019@cluster0-qsqrm.mongodb.net/blognode?retryWrites=true&w=majority'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
